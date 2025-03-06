6a9791374751bdd87c3a6b19c1b0a1781fd1e548
Elton da Costa Oliveira <eltoncosta00614@gmail.com>
2025-03-06 18:03:47 -0300

    feat: add gravity

--- script.js
+++ script.js
@@ -9,15 +9,36 @@ function control(direction) {
     let int_position_y = parseInt(position_y.replace("px", ""))
     
     // adicionando valor à posição atual do jogador para se mover
+    let speed = 10
     switch (direction) {
         case "left":
-            character.style.left = int_position_x - 10 + "px"
+            character.style.left = int_position_x - speed + "px"
             break
         case "right":
-            character.style.left = int_position_x + 10 + "px"
+            character.style.left = int_position_x + speed + "px"
             break
         case "up":
-            character.style.top = int_position_y - 10 + "px"
+            character.style.top = int_position_y - speed + "px"
             break
     }
+}
+
+setInterval(gravity, 250)
+
+
+function gravity() {
+    // pegando o valor posicional Y do jogador para ir diminuindo e dar sensação de gravidade
+    let character = document.getElementById("character")
+
+    let position_y = getComputedStyle(character).top
+    let int_position_y = parseInt(position_y.replace("px", ""))
+    console.log(int_position_y)
+
+    let gravity = 10
+
+    if (int_position_y < 489) {
+        character.style.top = int_position_y + gravity + "px"
+    }
+
+
 }
